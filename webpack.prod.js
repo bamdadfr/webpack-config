35a968ca65ca3005c4304fd40a2660afb0871e77
Bamdad Sabbagh <git@bamdadsabbagh.com>
2020-07-08 10:47:18 +0200

    fix: merge import [force-release]

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,4 +1,4 @@
-const merge = require ('webpack-merge')
+const { merge } = require ('webpack-merge')
 const MiniCssExtractPlugin = require ('mini-css-extract-plugin')
 const TerserJSPlugin = require ('terser-webpack-plugin')
 const OptimizeCSSAssetsPlugin = require ('optimize-css-assets-webpack-plugin')
